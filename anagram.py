1215e5ed90f6fccd77a45dc8bf431c831b92d72e
tohmyy <71967643+tohmyy@users.noreply.github.com>
2022-05-28 21:04:00 +0100

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,24 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    # Strings are sorted and verified
+    if(sorted(word)== sorted(anagram)):
+        return True
+    else:
+        return False
+
+# driver code
+print("enter first word ")
+word =input()
+print("enter second word (anagram) ")
+anagram =input()
+print(find_anagram(word, anagram))
+
+    
+
